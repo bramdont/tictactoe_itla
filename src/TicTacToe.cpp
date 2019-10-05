fbed0a264112bd22dc8902f5093fb7b005d9551c
Abhishek Khurana <abhishekkhurana12345@gmail.com>
2019-10-05 23:10:57 +0530

    Reduced the game over checking code segment

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -53,37 +53,16 @@ bool gameover()
 
     if (plays > 5)
     {
-        if (board [0][0] == board [0][1] && board [0][1] == board [0][2] && board [0][1] != '_')
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-         else if (board [1][0] == board [1][1] && board [1][1] == board [1][2] && board [1][1] != '_')
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-        else if (board [2][0] == board [2][1] && board [2][1] == board [2][2] && board [2][1] != '_')
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-        else if (board [0][0] == board [1][0] && board [1][0] == board [2][0] && board [1][0])
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-        else if (board [0][1] == board [1][1] && board [1][1] == board [2][1] && board [1][1] != '_')
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-        else if (board [0][2] == board [1][2] && board [1][2] == board [2][2] && board [1][2] != '_')
-        {
-            cout << "Player " << winner << " win." << endl;
-            return true;
-        }
-        else if (board [0][0] == board [1][1] && board [1][1] == board [2][2] & board [1][1] != '_')
+		
+		for (unsigned short i = 0; i < 3; ++i) {
+			if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != '_')||(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != '_'));
+			{
+				cout << "Player " << winner << " win." << endl;
+				return true;
+			}
+		}
+       
+        if (board [0][0] == board [1][1] && board [1][1] == board [2][2] & board [1][1] != '_')
         {
             cout << "Player " << winner << "win." << endl;
             return true;
